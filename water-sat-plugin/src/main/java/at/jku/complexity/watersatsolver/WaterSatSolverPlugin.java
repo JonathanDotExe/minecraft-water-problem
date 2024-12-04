d7e7219d2da82b252f821705cec57223caef0c93
DatDonathan <jojo08082003@gmail.com>
2024-12-04 11:20:00 +0100

    Add plugin structure

---
+++ water-sat-plugin/src/main/java/at/jku/complexity/watersatsolver/WaterSatSolverPlugin.java
@@ -0,0 +1,14 @@
+package at.jku.complexity.watersatsolver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class WaterSatSolverPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+}
